8723d6041752514214a28929206ce6a55ab01996
Naitsirc <theberzerker_15_@hotmail.com>
2018-05-12 18:56:00 -0300

    routes noticia del pequeño avance.

--- server/routes/noticia.js
+++ server/routes/noticia.js
@@ -4,6 +4,9 @@
  */
 
 'use strict';
+const NoticiaController =require('../controller/noticia');
+
 const express=require('express');
 const api=express.Router();
+api.put('/nsave',NoticiaController.guardarNoticia);
 module.exports=api;
